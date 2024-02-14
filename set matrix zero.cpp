199f935374ed64dd09757eb6da6fc185d22b35dd
ROHIT RANA <83514950+Rohitrana2429@users.noreply.github.com>
2024-02-14 12:20:20 +0530

    Create set matrix zero.cpp

---
+++ set matrix zero.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+class Solution {
+public:
+    void setZeroes(std::vector<std::vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        std::vector<int> row(m, 0); // Initialize row vector with m elements, all set to 0
+        std::vector<int> col(n, 0); // Initialize col vector with n elements, all set to 0
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
